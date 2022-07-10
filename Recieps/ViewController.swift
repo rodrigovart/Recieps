51e5e3349458c39e7ee552c3cf5bf89be10b3ffc
Rodrigo Vart <rodrigo.vart@capgemini.com>
2022-07-10 14:18:56 -0300

    commit inicial do projeto

---
+++ Recieps/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Recieps
+//
+//  Created by Rodrigo Vart on 10/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
